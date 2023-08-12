58d38540968321694007beb36f7a371dd0f287ab
cresssss <129712643+cresssss@users.noreply.github.com>
2023-08-12 15:03:21 -0400

    Update PlayerYouth.h

--- PlayerYouth.h
+++ PlayerYouth.h
@@ -1,14 +1,14 @@
-#ifndef PLAYERYOUTH_H
-#define PLAYERYUTH_H
-
+#ifndef YOUTHPLAYER_H
+#define YOUTHPLAYER_H
 
 #include "Player.h"
 
-class YouthPlayer : public Player
+template<typename T>
+class YouthPlayer : public Player<T>
 {
 private:
     int age;
-    std::vector<double> attack_cards;
+    std::vector<T> attack_cards;
 
 public:
     YouthPlayer();
@@ -18,14 +18,12 @@ public:
     void set_age(int age);
     int get_age() const;
 
-    void add_attack_card(double card);
-    void add_attack_cards(const std::vector<double>& cards);
-    double sum_attack_cards() const;
-    double sum_all_cards() const;
+    void add_attack_card(T card);
+    void add_attack_cards(const std::vector<T>& cards);
+    T sum_attack_cards() const;
+    T sum_all_cards() const;
 
     virtual ~YouthPlayer() override;
 };
 
 #endif // YOUTHPLAYER_H
-
-
