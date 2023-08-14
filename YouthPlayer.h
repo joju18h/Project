f47902c9319edb71c466a5a4d9db1a5988e64906
jask1023 <123787489+jask1023@users.noreply.github.com>
2023-08-13 21:26:09 -0400

    Final YouthPlayer.h

--- YouthPlayer.h
+++ YouthPlayer.h
@@ -2,7 +2,6 @@
 #define YOUTHPLAYER_H
 
 #include "Player.h"
-//#include "AdultPlayer.h"
 
 //Range constants for generating attack cards
 #define ATTACKMIN 0
@@ -11,32 +10,19 @@ template <typename T>
 class YouthPlayer : public Player<T>
 {
     int age;
-    int attackMax;
+    T attackMax;
     std::vector<T> attackCards;
 
 public:
+    //constructor
+    YouthPlayer(const std::string player, int age);
 
-   YouthPlayer<T>(const std::string player, int age) : Player<T>(player) {
-        if (age < 4) {
-            this->age = 4;
-        }
-
-        else if (age > 18) {
-            this->age = 19;
-        }
-
-        else this->age = age;
-
-        attackMax= this->age / 2;
-    }
-
-    Player<T>& operator=(const Player<T>& otherPlayer);
+    //overriding and overloading functions from base class
     void addAttackCard();
     void addAttackCard(int);
-
     T sumAttackCards() const;
     T sumAllCards() const;
-
+    YouthPlayer<T>& operator=(const YouthPlayer& otherPlayer);
     bool operator==(const Player<T>& p2);
     bool operator>(const Player<T>& p2);
     bool operator<(const Player<T>& p2);
@@ -46,23 +32,43 @@ public:
     ~YouthPlayer() {};
 };
 
-
+//constructor definition 
+//passes player name to Player base class
 template <typename T>
-Player<T>& YouthPlayer<T>::operator=(const Player<T>& otherPlayer)
-{
-    this->name = otherPlayer.getName();
-    this->cash = otherPlayer.getCash();
-    return *this;
+YouthPlayer<T>::YouthPlayer(const std::string player, int age) : Player<T>(player) {
+    if (age < 4) {
+        this->age = 4;
+        std::cout << "Age has been set to the minimum age of 4.\n";
+    }
+
+    else if (age > 18) {
+        this->age = 19;
+        std::cout << "Age has been set to the maximum age of 19.\n";
+    }
+
+    else this->age = age;
+
+    //set attack max to half the player's age
+    attackMax = this->age / 2;
 }
 
+//adding attack cards functions
 template <typename T>
 void YouthPlayer<T>::addAttackCard(int numCards)
 {
     this->attackCards.clear();
     srand(time(0));
-    for (int i = 0; i < numCards; i++)
-    {
-        this->attackCards.push_back((T) ((rand() / (T)RAND_MAX) + (rand() % attackMax)));
+    for (int i = 0; i < numCards; i++) {
+        if (std::is_integral<T>::value) {
+            //generates a random integer value between 
+            //1 to attackMax (inclusive)
+            this->attackCards.push_back(rand() % (int)attackMax+1);
+        }
+        else {
+            //generates a random floating point number
+            //between 0 and attackMax (inclusive)
+            this->attackCards.push_back(rand() / (double)RAND_MAX * attackMax);
+        }
     }
 }
 
@@ -70,9 +76,15 @@ template <typename T>
 void YouthPlayer<T>::addAttackCard()
 {
     srand(time(0));
-    this->attackCards.push_back((T) ((rand()/(T)RAND_MAX) + (rand() % attackMax)));
+    if (std::is_integral<T>::value) {
+        this->attackCards.push_back(rand() % (int)attackMax+1);
+    }
+    else {
+        this->attackCards.push_back((rand()) / (double)RAND_MAX * attackMax);
+    }
 }
 
+//summing cards functions
 template <typename T>
 T YouthPlayer<T>::sumAttackCards() const
 {
@@ -95,6 +107,7 @@ bool YouthPlayer<T>::operator==(const Player<T>& p2)
     return (this->sumAllCards() == p2.sumAllCards());
 };
 
+//overridden comparison operators
 template <typename T>
 bool YouthPlayer<T>::operator>(const Player<T>& p2)
 {
@@ -118,4 +131,12 @@ void YouthPlayer<T>::operator>>(double amt)
 {
     this->Player<T>::addCash(-amt);
 };
+
+//overloaded operator function
+template <typename T>
+YouthPlayer<T>& YouthPlayer<T>::operator=(const YouthPlayer& otherPlayer)
+{
+    *this = otherPlayer;
+}
+
 #endif
